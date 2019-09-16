eb4c9c9e86ef488a4da412e7c0bc9a081a1d904c
Prajwal Prashanth <prajapple@gmail.com>
2019-09-16 16:08:46 +0530

    changed the github repo issues link

--- jovian/utils/constants.py
+++ jovian/utils/constants.py
@@ -5,8 +5,8 @@ DEFAULT_API_URL = "https://api.jovian.ai/"
 RC_FILENAME = ".jovianrc"
 FILENAME_MSG = 'Failed to detect notebook filename. Please provide the notebook filename ' + \
     '(including .ipynb extension) as the "nb_filename" argument to "jovian.commit".'
-ISSUES_MSG = """NOTE: Jovian is current in beta, so if you face any issues, 
-      please report them here: https://github.com/swiftace-ai/jovian-py/issues"""
+ISSUES_MSG = """NOTE: Jovian is currently in beta, so if you face any issues, 
+      please report them here: https://github.com/jvn-io/jovian-py/issues"""
 
 LINUX = 'linux'
 WINDOWS = 'windows'
