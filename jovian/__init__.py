a81d254a7aaaf006d94017d521b95612d8e7226e
Aakash N S <aakashns.sky@gmail.com>
2019-12-23 15:44:38 +0530

    update callbacks

--- jovian/__init__.py
+++ jovian/__init__.py
@@ -1,11 +1,10 @@
 
 from jovian._version import __version__
-from jovian.utils.jupyter import set_notebook_name
-from jovian.utils.latest import check_update
+
+from jovian.utils.initialize import _initialize_jovian
 from jovian.utils.commit import commit
 from jovian.utils.records import log_hyperparams, log_metrics, log_dataset, log_git, reset_records
 from jovian.utils.slack import notify
 
 
-set_notebook_name()
-check_update()
+_initialize_jovian()
