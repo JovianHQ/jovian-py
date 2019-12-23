af273f040be0484800a3ee10df340d786c7407bd
Rohit Sanjay <sanjay.rohit2@gmail.com>
2019-12-23 23:44:34 +0530

    added tests for git.py

--- jovian/tests/utils/test_git.py
+++ jovian/tests/utils/test_git.py
@@ -1,27 +1,49 @@
 import os
 from unittest import TestCase
 
-from jovian.utils.git import is_git
+from jovian.utils.git import is_git, get_branch, get_remote, get_repository_root
 
 path = 'mock_git_repo'
 
 
 class GitSetup(TestCase):
-    @classmethod
+    path = 'mock_git_repo'
+    origin_url = 'https://github.com/rohitsanj/mock_repo.git'
+
     def setUp(self):
-        if not os.path.exists(path):
-            os.mkdir(path)
-            os.chdir(path)
+        os.mkdir(self.path)
+        os.chdir(self.path)
         os.system('git init')
+        os.system('git remote add origin ' + self.origin_url)
         os.system('touch sample.txt && git add . && git commit -m "initial commit"')
 
-    @classmethod
     def tearDown(self):
-        os.system('cd -')
-        os.system('rm -rf ' + path)
+        os.chdir('..')
+        os.system('rm -rf ' + self.path)
 
 
 class TestIsGit(GitSetup):
 
     def test_is_git(self):
         self.assertTrue(is_git())
+
+
+class TestGetBranch(GitSetup):
+
+    def test_get_branch(self):
+        expected_result = "master"
+        self.assertEqual(get_branch(), expected_result)
+
+
+class TestGetRemote(GitSetup):
+
+    def test_get_remote(self):
+        expected_result = 'https://github.com/rohitsanj/mock_repo'
+        self.assertEqual(get_remote(), expected_result)
+
+
+class TestGetRepositoryRoot(GitSetup):
+
+    def test_get_repository_root(self):
+        expected_result = os.path.join(os.path.abspath('..'), path)
+        self.assertEqual(get_repository_root(), expected_result)
