1f53183657fc3a066c01b209c3304b8d4726007b
Rohit Sanjay <sanjay.rohit2@gmail.com>
2019-12-25 00:13:52 +0530

    made the changes in rc_file.py

--- jovian/tests/utils/test_rcfile.py
+++ jovian/tests/utils/test_rcfile.py
@@ -1,3 +1,5 @@
+import os
+import json
 from unittest import TestCase, mock
 from jovian.utils.constants import RC_FILENAME
 from jovian.utils.rcfile import (rcfile_exists, save_rcdata, get_rcdata,
@@ -11,68 +13,103 @@ data = {
     }
 }
 
-json_data = """{
-    "notebooks": {
-        "Testing Jovian.ipynb": {
-            "slug": "46bd9a3f87e74de0baf8a6f0b60a8df9"
-        }
-    }
-}"""
+
+class RCFile(TestCase):
+    def setUp(self):
+        os.chdir('jovian/tests/resources/rcfile')
+
+    def tearDown(self):
+        os.chdir('../' * 4)
 
 
-class TestRCFileExists(TestCase):
+class CreateNewRCFile(RCFile):
+    def setUp(self):
+        super().setUp()
+        with open(RC_FILENAME, 'w') as f:
+            json.dump(data, f, indent=2)
 
-    @mock.patch("jovian.utils.rcfile.os")
-    def test_rcfile_exists(self, mock_os):
-        rcfile_exists()
-        mock_os.path.exists.assert_called_with(RC_FILENAME)
+    def tearDown(self):
+        os.system('rm .jovianrc')
+        super().tearDown()
 
 
-class TestSaveRCData(TestCase):
+class TestRCFileExists(RCFile):
 
-    @mock.patch("jovian.utils.rcfile.open", new_callable=mock.mock_open)
-    @mock.patch("jovian.utils.rcfile.json.dump")
-    def test_save_rcdata_none(self, mock_json, mock_file):
+    def setUp(self):
+        super().setUp()
+        os.system('touch .jovianrc')
+
+    def test_rcfile_exists(self):
+        self.assertTrue(rcfile_exists())
+
+    def tearDown(self):
+        os.system('rm .jovianrc')
+        super().tearDown()
+
+
+class TestRCFileDoesNotExist(RCFile):
+
+    def test_rcfile_does_not_exists(self):
+        self.assertFalse(rcfile_exists())
+
+
+class TestSaveRCData(RCFile):
+
+    def test_save_rcdata_data_none(self):
         save_rcdata(data=None)
-        mock_json.assert_called_with({'notebooks': {}}, mock_file.return_value, indent=2)
+        expected_result = {
+            "notebooks": {}
+        }
+        with open(RC_FILENAME, 'r') as f:
+            self.assertEqual(json.load(f), expected_result)
 
-    @mock.patch("jovian.utils.rcfile.open", new_callable=mock.mock_open)
-    @mock.patch("jovian.utils.rcfile.json.dump")
-    def test_save_rcdata_normal(self, mock_json, mock_file):
+    def test_save_rcdata_data_exists(self):
         save_rcdata(data=data)
-        mock_json.assert_called_with(data, mock_file.return_value, indent=2)
+        expected_result = data
+
+        with open(RC_FILENAME, 'r') as f:
+            self.assertEqual(json.load(f), expected_result)
 
 
-class TestGetRCData(TestCase):
+class TestGetRCData(RCFile):
 
-    @mock.patch("jovian.utils.rcfile.rcfile_exists", mock.Mock(return_value=True))
-    @mock.patch("jovian.utils.rcfile.json.loads")
-    @mock.patch("jovian.utils.rcfile.open", new_callable=mock.mock_open, read_data=json_data)
-    def test_save_get_rcdata_true(self, mock_open, mock_loads):
-        self.assertEqual(get_rcdata(), mock_loads.return_value)
+    def test_save_get_rcdata_rcfile_does_not_exist(self):
+        expected_result = {
+            "notebooks": {}
+        }
+        self.assertEqual(get_rcdata(), expected_result)
 
-    @mock.patch("jovian.utils.rcfile.rcfile_exists", mock.Mock(return_value=False))
-    @mock.patch("jovian.utils.rcfile.json.loads")
-    @mock.patch("jovian.utils.rcfile.open", new_callable=mock.mock_open, read_data=json_data)
-    def test_save_get_rcdata_false(self, mock_open, mock_loads):
-        self.assertEqual(get_rcdata(), mock_loads.return_value)
+    def test_save_get_rcdata_rcfile_exists(self):
+        with open(RC_FILENAME, 'w') as f:
+            json.dump(data, f, indent=2)
+
+        expected_result = data
+
+        self.assertEqual(get_rcdata(), expected_result)
+
+    def tearDown(self):
+        os.system('rm .jovianrc')
+        super().tearDown()
 
 
-class TestGetNotebookSlug(TestCase):
+class TestGetNotebookSlug(CreateNewRCFile):
 
-    @mock.patch("jovian.utils.rcfile.get_rcdata", mock.Mock(return_value=data))
-    def test_get_notebook_slug(self):
+    def test_get_notebook_slug_notebook_present(self):
         filename = "Testing Jovian.ipynb"
         expected_result = "46bd9a3f87e74de0baf8a6f0b60a8df9"
 
         self.assertEqual(get_notebook_slug(filename), expected_result)
 
+    def test_get_notebook_slug_notebook_not_present(self):
+        filename = "Testing Jovian123.ipynb"
+        expected_result = None
 
-class TestSetNotebookSlub(TestCase):
+        self.assertEqual(get_notebook_slug(filename), expected_result)
 
-    @mock.patch("jovian.utils.rcfile.get_rcdata", mock.Mock(return_value=data))
-    @mock.patch("jovian.utils.rcfile.save_rcdata")
-    def test_set_notebook_slug(self, mock_save_rcdata):
+
+class TestSetNotebookSlug(CreateNewRCFile):
+
+    def test_set_notebook_slug_new_entry(self):
         filename = "Testing Jovian 2.ipynb"
         slug = "46bd9a3f87e74de0baf8a6f0b60a8df9"
 
@@ -88,21 +125,22 @@ class TestSetNotebookSlub(TestCase):
         }
 
         set_notebook_slug(filename, slug)
-        mock_save_rcdata.assert_called_with(expected_result)
+        self.assertEqual(get_rcdata(), expected_result)
 
-
-class TestMakeRCData(TestCase):
-
-    @mock.patch("jovian.utils.rcfile.json.dumps")
-    def test_make_rcdata(self, mock_dumps):
+    def test_set_notebook_slug_existing_entry(self):
         filename = "Testing Jovian.ipynb"
         slug = "46bd9a3f87e74de0baf8a6f0b60a8df9"
-        expected_result = {
-            "notebooks": {
-                filename: {
-                    "slug": slug
-                }
-            }
-        }
-        make_rcdata(filename, slug)
-        mock_dumps.assert_called_with(expected_result)
+
+        expected_result = data
+
+        set_notebook_slug(filename, slug)
+        self.assertEqual(get_rcdata(), expected_result)
+
+
+class TestMakeRCData(CreateNewRCFile):
+    def test_make_rcdata(self):
+        filename = "Testing Jovian.ipynb"
+        slug = "46bd9a3f87e74de0baf8a6f0b60a8df9"
+        expected_result = '{"notebooks": {"Testing Jovian.ipynb": {"slug": "46bd9a3f87e74de0baf8a6f0b60a8df9"}}}'
+
+        self.assertEqual(make_rcdata(filename, slug), expected_result)
