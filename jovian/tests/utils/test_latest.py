6a13607ef6cd98a328ff505e23cee5b2ae3f7cd9
Rohit Sanjay <sanjay.rohit2@gmail.com>
2019-12-24 17:43:27 +0530

    added test cases for latest.py

---
+++ jovian/tests/utils/test_latest.py
@@ -0,0 +1,43 @@
+from unittest import TestCase, mock
+from jovian._version import __version__
+
+from jovian.utils.latest import _get_latest_version, _print_update_message, check_update
+
+
+class TestGetLatestVersion(TestCase):
+    def error_raiser(*args, **kwargs):
+        raise ImportError('fake requests.get error')
+
+    @mock.patch("jovian.utils.latest.requests.get", mock.Mock(return_value=error_raiser))
+    def test_get_latest_version_exception(self):
+        expected_result = __version__
+        self.assertEqual(_get_latest_version(), expected_result)
+
+
+class TestPrintUpdateMessage(TestCase):
+
+    @mock.patch("jovian.utils.latest.log")
+    def test_print_update_message_normal(self, mock_log):
+        current_version = '0.0.1'
+        latest_version = '0.0.2'
+        expected_result = 'Update Available: {0} --> {1}'.format(current_version, latest_version)
+        _print_update_message(current_version, latest_version)
+        mock_log.assert_any_call(expected_result)
+
+    @mock.patch("jovian.utils.latest.in_notebook", mock.Mock(return_value=True))
+    @mock.patch("jovian.utils.latest.log")
+    def test_print_update_message_in_notebook(self, mock_log):
+        current_version = '0.0.1'
+        latest_version = '0.0.2'
+        expected_result = 'Run `!pip install jovian --upgrade` to upgrade'
+        _print_update_message(current_version, latest_version)
+        mock_log.assert_any_call(expected_result)
+
+    @mock.patch("jovian.utils.latest.in_notebook", mock.Mock(return_value=False))
+    @mock.patch("jovian.utils.latest.log")
+    def test_print_update_message_not_in_notebook(self, mock_log):
+        current_version = '0.0.1'
+        latest_version = '0.0.2'
+        expected_result = 'Run `pip install jovian --upgrade` to upgrade\n'
+        _print_update_message(current_version, latest_version)
+        mock_log.assert_any_call(expected_result)
