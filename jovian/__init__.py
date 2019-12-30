51c2163fd8231274b049e049f1b5a63e6dd5a1d3
Aakash N S <aakashns.sky@gmail.com>
2019-12-30 15:24:09 +0530

    Improve logging API with **kwargs

--- jovian/__init__.py
+++ jovian/__init__.py
@@ -1,7 +1,7 @@
 
 from jovian._version import __version__
 from jovian.utils.commit import commit
-from jovian.utils.records import log_hyperparams, log_metrics, log_dataset, log_git, reset_records
+from jovian.utils.records import log_hyperparams, log_metrics, log_dataset, log_git, reset
 from jovian.utils.slack import notify
 from jovian.utils.configure import reset_config, configure
 from jovian.utils.initialize import _initialize_jovian
