bc2f7c1e1e69a6ac0c3ee5bfa833b841386af51f
Rohit Sanjay <sanjay.rohit2@gmail.com>
2020-01-08 20:13:41 +0530

    Unit Testing (#92)
    * removed main of test_envfile.py for consistency
    
    * misc.py 100% coverage
    
    * refactor test_envfile.py
    
    * testing envfile.py - 84%
    
    * mock random in latest.p
    
    * removed redundant code in envfile.py
    
    * assert get and post called once and twice

--- jovian/tests/utils/test_envfile.py
+++ jovian/tests/utils/test_envfile.py
@@ -2,47 +2,73 @@ import os
 from unittest import TestCase
 from jovian.utils.envfile import (check_error, extract_env_name, extract_env_packages, extract_package_from_line,
                                   extract_pip_packages, get_environment_dict, identify_env_file,
-                                  dump_environment_to_yaml_file, write_env_name)
+                                  dump_environment_to_yaml_file, write_env_name, remove_packages,
+                                  sanitize_envfile, serialize_packages)
 
-FILES_PREFIX = 'jovian/tests/resources/yaml/'
+
+class EnvFile(TestCase):
+    def setUp(self):
+        os.chdir('jovian/tests/resources/yaml')
+
+    def tearDown(self):
+        os.chdir('../' * 4)
 
 
-class InstallUtilsTest(TestCase):
+class TestIdentityEnvFile(EnvFile):
 
-    def test_identify_env_file(self):
-        env_fname = identify_env_file(env_fname=None, folder_prefix=FILES_PREFIX)
-        self.assertEqual(env_fname, FILES_PREFIX + 'environment.yml')
+    def test_identify_env_file_no_environment_file(self):
+        self.assertIsNone(identify_env_file(env_fname=None))
 
+    def test_identify_env_file_exists(self):
+        os.system('touch environment.yml')
+
+        self.assertEqual(identify_env_file(env_fname=None), 'environment.yml')
+
+        os.system('rm environment.yml')
+
+
+class TestGetEnvironmentDict(EnvFile):
     def test_get_environment_dict(self):
-        env_filename = FILES_PREFIX + 'environment.yml'
+        expected_result = {'prefix': '/home/admin/anaconda3/envs/test-env',
+                           'dependencies': ['mixpanel=1.11.0', 'sigmasix=1.91.0', 'sqlite',
+                                            {'pip': ['six==1.11.0', 'sqlite==2.0.0']}],
+                           'channels': ['defaults'], 'name': 'test-env'}
+        environment = get_environment_dict(env_fname='environment-test.yml')
+
+        self.assertDictEqual(environment, expected_result)
+
+    def test_get_environment_dict_non_existent_file(self):
         expected = {'prefix': '/home/admin/anaconda3/envs/test-env',
                     'dependencies': ['mixpanel=1.11.0', 'sigmasix=1.91.0', 'sqlite',
                                      {'pip': ['six==1.11.0', 'sqlite==2.0.0']}],
                     'channels': ['defaults'], 'name': 'test-env'}
-        environment = get_environment_dict(env_fname=env_filename)
-
-        self.assertDictEqual(environment, expected)
+        environment = get_environment_dict(env_fname='environment-test.yml')
 
         with self.assertRaises(FileNotFoundError):
             get_environment_dict(env_fname='non-existent-file.yml')
 
-        # with self.assertRaises(yaml.YAMLError):    # we're printing the exception, not raising it.
-        #     get_environment_dict(env_fname=FILES_PREFIX + 'invalid-yaml-file.yml')
+    def test_get_environment_dict_raises_exception_invalid_file(self):
+        expected_result = {}
 
+        self.assertDictEqual(get_environment_dict('invalid-yaml-file.yml'), expected_result)
+
+
+class TestExtractEnvName(EnvFile):
     def test_extract_env_name(self):
-        env_filename = FILES_PREFIX + 'environment.yml'
-        name = extract_env_name(env_fname=env_filename)
-        name2 = extract_env_name(env_fname=FILES_PREFIX+'empty-yaml-file.yml')
+        name = extract_env_name(env_fname='environment-test.yml')
+        name2 = extract_env_name(env_fname='empty-yaml-file.yml')
 
         self.assertEqual(name, 'test-env')
         self.assertIsNone(name2)
         with self.assertRaises(FileNotFoundError):
             extract_env_name(env_fname='non-existent-file.yml')
 
+
+class TestExtractEnvPackages(EnvFile):
     def test_extract_env_packages(self):
-        env_filename = FILES_PREFIX + 'environment.yml'
-        dep = extract_env_packages(env_fname=env_filename)
-        dep2 = extract_env_packages(env_fname=FILES_PREFIX + 'empty-yaml-file.yml')
+
+        dep = extract_env_packages(env_fname='environment-test.yml')
+        dep2 = extract_env_packages(env_fname='empty-yaml-file.yml')
 
         self.assertListEqual(dep, ['mixpanel=1.11.0', 'sigmasix=1.91.0', 'sqlite',
                                    'six==1.11.0', 'sqlite==2.0.0'])
@@ -50,20 +76,22 @@ class InstallUtilsTest(TestCase):
         with self.assertRaises(FileNotFoundError):
             extract_env_packages(env_fname='non-existent-file.yml')
 
+
+class TestExtractPipPackages(EnvFile):
     def test_extract_pip_packages(self):
-        env_filename = FILES_PREFIX + 'environment.yml'
-        env_filename2 = FILES_PREFIX + 'empty-yaml-file.yml'
-        dep = extract_pip_packages(env_fname=env_filename)
-        dep2 = extract_pip_packages(env_fname=env_filename2)
+        dep = extract_pip_packages(env_fname='environment-test.yml')
+        dep2 = extract_pip_packages(env_fname='empty-yaml-file.yml')
 
         self.assertListEqual(dep, ['six==1.11.0', 'sqlite==2.0.0'])
         self.assertListEqual(dep2, [])
         with self.assertRaises(FileNotFoundError):
             extract_env_packages(env_fname='non-existent-file.yml')
 
+
+class TestCheckError(EnvFile):
     def test_check_error(self):
-        env_filename = FILES_PREFIX + 'environment.yml'
-        packages = extract_env_packages(env_fname=env_filename)
+
+        packages = extract_env_packages(env_fname='environment-test.yml')
 
         error_str = '''ResolvePackageNotFound: 
                         - mixpanel=1.11.0'''
@@ -82,9 +110,10 @@ class InstallUtilsTest(TestCase):
         self.assertIsNone(error3)
         self.assertListEqual(pkgs3, [])
 
+
+class TestExtractPackageFromLine(EnvFile):
     def test_extract_package_from_line(self):
-        env_filename = FILES_PREFIX + 'environment.yml'
-        packages = extract_env_packages(env_fname=env_filename)
+        packages = extract_env_packages(env_fname='environment-test.yml')
 
         lines = [['- mixpanel=1.11.0', 'mixpanel=1.11.0'], ['sqlite<3x not compatible', 'sqlite'],
                  ['', None], ['line containing six < 3.0.x', 'six==1.11.0']]
@@ -92,59 +121,21 @@ class InstallUtilsTest(TestCase):
             self.assertEqual(extract_package_from_line(line=line[0], packages=packages), line[1])
 
 
-class EnvFile(TestCase):
-    def setUp(self):
-        os.chdir('jovian/tests/resources/yaml')
-
-    def tearDown(self):
-        os.chdir('../' * 4)
-
-
-class TestGetEnvironmentDict(EnvFile):
-
-    def test_get_environment_dict(self):
-        expected_result = {'channels': ['defaults'],
-                           'dependencies': ['mixpanel=1.11.0',
-                                            'sigmasix=1.91.0',
-                                            'sqlite',
-                                            {'pip': ['six==1.11.0', 'sqlite==2.0.0']}],
-                           'name': 'test-env',
-                           'prefix': '/home/admin/anaconda3/envs/test-env'}
-
-        self.assertEqual(get_environment_dict('environment.yml'), expected_result)
-
-    def test_get_environment_dict_invalid_yml(self):
-        expected_result = {}
-
-        self.assertEqual(get_environment_dict('invalid-yaml-file.yml'), expected_result)
-
-    def test_get_environment_dict_raises_exception_invalid_file(self):
-        expected_result = {}
-
-        self.assertEqual(get_environment_dict('invalid-yaml-file.yml'), expected_result)
-
-    def test_get_environment_dict_raises_exception_non_existent_file(self):
-        with self.assertRaises(FileNotFoundError):
-            get_environment_dict('non-existent-file.yml')
-
-    def test_get_environment_dict_raises_exception_empty_file(self):
-        expected_result = None
-
-        self.assertEqual(get_environment_dict('empty-yaml-file.yml'), expected_result)
-
-
 class TestDumpEnvironmentToYamlFile(EnvFile):
-    def test_dump_environment_to_yaml_file_normal(self):
-        data = {'channels': ['defaults'],
-                'dependencies': ['mixpanel=1.11.0',
-                                 'sigmasix=1.91.0',
-                                 'sqlite',
-                                 {'pip': ['six==1.11.0', 'sqlite==2.0.0']}],
-                'name': 'test-env',
-                'prefix': '/home/admin/anaconda3/envs/test-env'}
-        dump_environment_to_yaml_file('test.yml', data)
+    data = {'channels': ['defaults'],
+            'dependencies': ['mixpanel=1.11.0',
+                             'sigmasix=1.91.0',
+                             'sqlite',
+                             {'pip': ['six==1.11.0', 'sqlite==2.0.0']}],
+            'name': 'test-env',
+            'prefix': '/home/admin/anaconda3/envs/test-env'}
 
-        self.assertEqual(get_environment_dict('test.yml'), data)
+    def setUp(self):
+        super().setUp()
+        dump_environment_to_yaml_file('test.yml', self.data)
+
+    def test_dump_environment_to_yaml_file_normal(self):
+        self.assertEqual(get_environment_dict('test.yml'), self.data)
 
     def tearDown(self):
         os.system('rm test.yml')
@@ -193,5 +184,62 @@ class TestWriteEnvName(EnvFile):
         super().tearDown()
 
 
-if __name__ == '__main__':
-    unittest.main()
+class TestRemovePackages(EnvFile):
+
+    def test_remove_packages(self):
+        packages = extract_env_packages(env_fname='environment-test.yml')
+        dependencies = ['dep1', 'dep2', 'mixpanel=1.11.0', 'sqlite']
+
+        expected_result = ['dep1', 'dep2']
+
+        self.assertEqual(remove_packages(dependencies, packages), expected_result)
+
+    def test_remove_packages_pip(self):
+        packages = extract_env_packages(env_fname='environment-test.yml')
+        dependencies = ['dep1', 'mixpanel=1.11.0', {'pip': ['six==1.11.0', 'dep3']}]
+
+        expected_result = ['dep1', {'pip': ['dep3']}]
+
+        self.assertEqual(remove_packages(dependencies, packages), expected_result)
+
+
+class TestSanitizeEnvfile(EnvFile):
+    data = {'channels': ['defaults'],
+            'dependencies': ['mixpanel=1.11.0',
+                             'sigmasix=1.91.0',
+                             'sqlite',
+                             {'pip': ['six==1.11.0', 'sqlite==2.0.0']}],
+            'name': 'test-env',
+            'prefix': '/home/admin/anaconda3/envs/test-env'}
+
+    def setUp(self):
+        super().setUp()
+        dump_environment_to_yaml_file('test.yml', self.data)
+
+    def test_sanitize_envfile(self):
+        packages = ['six==1.11.0', 'sigmasix=1.91.0']
+        sanitize_envfile('test.yml', packages)
+
+        expected_result = {'channels': ['defaults'],
+                           'dependencies': ['mixpanel=1.11.0',
+                                            'sqlite',
+                                            {'pip': ['sqlite==2.0.0']}],
+                           'name': 'test-env',
+                           'prefix': '/home/admin/anaconda3/envs/test-env'}
+        self.assertEqual(get_environment_dict('test.yml'), expected_result)
+
+    def tearDown(self):
+        os.system('rm test.yml')
+        super().tearDown()
+
+
+class TestSerializePackages(EnvFile):
+
+    def test_serialize_packages(self):
+        dependencies = ['dep1', 'mixpanel=1.11.0', {'pip': ['six==1.11.0', 'dep3']}]
+
+        expected_result = ['dep1', 'mixpanel=1.11.0', 'six==1.11.0', 'dep3']
+
+        self.assertEqual(serialize_packages(dependencies), expected_result)
+
+# class Test
--- jovian/tests/utils/test_latest.py
+++ jovian/tests/utils/test_latest.py
@@ -5,9 +5,18 @@ from pkg_resources import parse_version
 from jovian.utils.latest import _get_latest_version, _print_update_message, check_update
 
 
-@mock.patch("requests.get", mock.Mock(return_value="0.2.0"))
+class MockResponse:
+    def __init__(self, json_data, status_code):
+        self.json_data = json_data
+        self.status_code = status_code
+
+    def json(self):
+        return self.json_data
+
+
+@mock.patch("requests.get", mock.Mock(return_value=MockResponse({'info': {'version': '0.2.0.dev1'}}, 200)))
 def test_get_latest_version_normal():
-    expected_result = __version__
+    expected_result = "0.2.0.dev1"
     assert _get_latest_version() == expected_result
 
 
@@ -38,7 +47,7 @@ def test_print_update_message_not_in_notebook(capsys):
 
 
 @mock.patch("jovian.utils.latest._get_latest_version", mock.Mock(return_value='0.0.2'))
-def test_get_update(capsys):
+def test_check_update(capsys):
     expected_result = """[jovian] Update Available: 0.0.1 --> 0.0.2
 [jovian] Run `pip install jovian --upgrade` to upgrade"""
 
@@ -46,5 +55,32 @@ def test_get_update(capsys):
     jovian.utils.latest.__version__ = '0.0.1'
 
     check_update(1)
+    captured = capsys.readouterr()
+    assert captured.out.strip() == expected_result
+
+
+@mock.patch("jovian.utils.latest._get_latest_version", mock.Mock(return_value='0.0.2'))
+@mock.patch("jovian.utils.latest.random", mock.Mock(return_value=0.5))
+def test_check_update_mock_random(capsys):
+    expected_result = """[jovian] Update Available: 0.0.1 --> 0.0.2
+[jovian] Run `pip install jovian --upgrade` to upgrade"""
+
+    import jovian
+    jovian.utils.latest.__version__ = '0.0.1'
+
+    check_update()
+    captured = capsys.readouterr()
+    assert captured.out.strip() == expected_result
+
+
+@mock.patch("jovian.utils.latest._get_latest_version", mock.Mock(return_value='0.0.2'))
+@mock.patch("jovian.utils.latest.random", mock.Mock(return_value=0.9))
+def test_check_update_mock_random_no_output(capsys):
+    expected_result = ""
+
+    import jovian
+    jovian.utils.latest.__version__ = '0.0.1'
+
+    check_update()
     captured = capsys.readouterr()
     assert captured.out.strip() == expected_result
