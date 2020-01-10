953934dca15a5215052cf8fc245d4649cfe2a44b
Rohit Sanjay <sanjay.rohit2@gmail.com>
2020-01-11 01:20:12 +0530

    testing credentials.py

--- jovian/tests/utils/test_credentials.py
+++ jovian/tests/utils/test_credentials.py
@@ -1,7 +1,8 @@
 import os
 from contextlib import contextmanager
 from jovian.utils import credentials
-from jovian.utils.credentials import config_exists, purge_config
+from jovian.utils.credentials import (get_creds_path, config_exists, purge_config, init_config, purge_creds, read_creds,
+                                      creds_exist, read_cred, write_creds, purge_cred_key)
 
 
 @contextmanager
@@ -33,5 +34,90 @@ def test_purge_config():
         purge_config()
         assert config_exists() == False
 
-# def test_read_creds_valid():
-#     with fake_creds('.jovian-purge', 'credentials.json'):
+
+def test_get_creds_path():
+    with fake_creds('.jovian', 'credentials.json'):
+        assert get_creds_path() == 'jovian/tests/resources/creds/.jovian/credentials.json'
+
+
+def test_init_config():
+    with fake_creds('.jovian-init-config', 'credentials.json'):
+        init_config()
+        assert os.path.exists('jovian/tests/resources/creds/.jovian-init-config') == True
+        purge_config()
+
+
+def test_purge_creds():
+    with fake_creds('.jovian-purge-creds', 'credentials.json'):
+        os.makedirs(credentials.CONFIG_DIR, exist_ok=True)
+        os.system('touch jovian/tests/resources/creds/.jovian-purge-creds/credentials.json')
+        assert os.path.exists('jovian/tests/resources/creds/.jovian-purge-creds/credentials.json') == True
+        purge_creds()
+        assert os.path.exists('jovian/tests/resources/creds/.jovian-purge-creds/credentials.json') == False
+        purge_config()
+
+
+def test_read_creds_no_creds_folder():
+    with fake_creds('.jovian-no-creds', 'credentials.json'):
+        assert read_creds() == {}
+
+
+def test_read_creds_folder_exists():
+    with fake_creds('.jovian', 'credentials.json'):
+        expected_result = {"WEBAPP_URL": "https://staging.jovian.ml/",
+                           "GUEST_KEY": "b6538d4dfde04fcf993463a828a9cec6",
+                           "ORG_ID": "staging",
+                           "API_URL": "https://api-staging.jovian.ai"}
+        assert read_creds() == expected_result
+
+
+def test_creds_exist_true():
+    with fake_creds('.jovian', 'credentials.json'):
+        assert creds_exist() == True
+
+
+def test_creds_exist_false():
+    with fake_creds('.jovian', 'credentials-no-api-key.json'):
+        assert creds_exist() == False
+
+
+def test_read_cred_no_default():
+    with fake_creds('.jovian', 'credentials.json'):
+        assert read_cred('ORG_ID') == 'staging'
+
+
+def test_read_cred_with_default():
+    with fake_creds('.jovian', 'credentials.json'):
+        assert read_cred('FAKE_ID', 'default_value') == 'default_value'
+
+
+def test_write_creds():
+    with fake_creds('.jovian-write-creds', 'credentials.json'):
+        creds = {"WEBAPP_URL": "https://staging.jovian.ml/",
+                 "GUEST_KEY": "b6538d4dfde04fcf993463a828a9cec6",
+                 "ORG_ID": "staging",
+                 "API_URL": "https://api-staging.jovian.ai"}
+        write_creds(creds)
+
+        assert read_creds() == creds
+
+        purge_config()
+
+
+def test_purge_cred_key():
+    with fake_creds('.jovian-write-creds', 'credentials.json'):
+        creds = {"WEBAPP_URL": "https://staging.jovian.ml/",
+                 "GUEST_KEY": "b6538d4dfde04fcf993463a828a9cec6",
+                 "ORG_ID": "staging",
+                 "API_URL": "https://api-staging.jovian.ai"}
+        write_creds(creds)
+
+        purge_cred_key('GUEST_KEY')
+
+        expected_result = {"WEBAPP_URL": "https://staging.jovian.ml/",
+                           "ORG_ID": "staging",
+                           "API_URL": "https://api-staging.jovian.ai"}
+
+        assert read_creds() == expected_result
+
+        purge_config()
