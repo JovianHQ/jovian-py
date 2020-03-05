6eba3766566df8150f506552b3c4d72d7a290130
Rohit Sanjay <sanjay.rohit2@gmail.com>
2020-03-05 15:51:48 +0530

    added test for fastai.py and created a req-dev added codecov
    edited req dev
    
    add cython
    
    added install option to cython
    
    refactor
    
    changed order
    
    added deps to pythonapp.yml directly
    
    added cython install
    
    Update pythonapp.yml
    
    Update pythonapp.yml
    
    Update pythonapp.yml
    
    added pytest conditional skip for test_fastai
    
    deal with fastai imports on py3.5
    
    deal with fastai imports on py3.5
    
    add codecov workflow
    
    modify pytest cov command
    
    fix coverage command
    
    added coverage workflow to same file

---
+++ jovian/tests/callbacks/test_fastai.py
@@ -0,0 +1,55 @@
+import sys
+from unittest import mock
+from unittest.mock import ANY
+
+import pytest
+try:
+    from fastai import *
+    from fastai.vision import *
+    from jovian.callbacks.fastai import JovianFastaiCallback
+except ImportError:
+    print('fastai needs Python 3.6 and above')
+    pass
+
+
+@pytest.fixture
+def learn():
+    mnist = untar_data(URLs.MNIST_TINY)
+    tfms = get_transforms(do_flip=False)
+
+    data = (ImageList.from_folder(mnist)
+            .split_by_folder()
+            .label_from_folder()
+            .transform(tfms, size=32)
+            .databunch()
+            .normalize(imagenet_stats))
+
+    data.show_batch()
+
+    learn = cnn_learner(data, models.resnet18, metrics=accuracy)
+    return learn
+
+
+@pytest.mark.skipif(sys.version_info < (3, 6), reason="requires python3.6 or higher")
+@mock.patch("jovian.callbacks.fastai.JovianFastaiCallback")
+@mock.patch("jovian.callbacks.fastai.log_hyperparams")
+@mock.patch("jovian.callbacks.fastai.log_metrics")
+def test_on_train_begin_and_on_epoch_end(mock_log_metrics, mock_log_hyperparams, mock_JovianFastaiCallback, learn):
+    jvn_cb = JovianFastaiCallback(learn, 'res18')
+    mock_JovianFastaiCallback.valid_set = False
+    learn.fit_one_cycle(1, callbacks=jvn_cb)
+
+    mock_log_hyperparams.assert_called_with({
+        'epochs': 1,
+        'batch_size': 64,
+        'loss_func': 'CrossEntropyLoss()',
+        'opt_func': 'torch.optim.adam.Adam',
+        'weight_decay': 0.01,
+        'learning_rate': '0.003',
+        'arch_name': 'res18'})
+
+    mock_log_metrics.assert_called_with({
+        'epoch': 0,
+        'train_loss': ANY,
+        'valid_loss': ANY,
+        'accuracy': ANY})
