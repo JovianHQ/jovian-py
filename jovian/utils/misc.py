a5a6cb475c63711046d2482838230adcc368bf00
Rohit Sanjay <sanjay.rohit2@gmail.com>
2020-04-18 12:47:12 +0530

    removed unnecessary import

--- jovian/utils/misc.py
+++ jovian/utils/misc.py
@@ -2,7 +2,6 @@ import time
 import platform
 
 from uuid import UUID
-from jovian.utils.credentials import read_creds
 from jovian.utils.constants import LINUX, WINDOWS, MACOS
 from jovian._version import __version__
 
