ccacb301b49375bacfa56d8e2e89b50c5bee5059
Rohit Sanjay <sanjay.rohit2@gmail.com>
2020-04-20 19:05:38 +0530

    refactor test_extension

--- jovian/tests/utils/test_extension.py
+++ jovian/tests/utils/test_extension.py
@@ -1,16 +1,19 @@
 import os
 from unittest import TestCase, mock
 
+import pytest
+
 from jovian.utils.extension import setup_extension
 
 
-class TestSetupExtension(TestCase):
-    @mock.patch('os.system')
-    def test_setup_extension_true(self, mock_system):
-        setup_extension(True)
-        mock_system.assert_called_with("jupyter nbextension enable jovian_nb_ext/main --sys-prefix")
-
-    @mock.patch('os.system')
-    def test_setup_extension_false(self, mock_system):
-        setup_extension(False)
-        mock_system.assert_called_with("jupyter nbextension disable jovian_nb_ext/main --sys-prefix")
+@pytest.mark.parametrize(
+    "enable, expected_result",
+    [
+        (True, "jupyter nbextension enable jovian_nb_ext/main --sys-prefix"),
+        (False, "jupyter nbextension disable jovian_nb_ext/main --sys-prefix"),
+    ]
+)
+@mock.patch('os.system')
+def test_setup_extension_true(mock_system, enable, expected_result):
+    setup_extension(enable=enable)
+    mock_system.assert_called_with(expected_result)
