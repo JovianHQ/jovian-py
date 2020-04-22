af1f34365e1b335125f3836bf16e5c1b96c7ac2e
Rohit Sanjay <sanjay.rohit2@gmail.com>
2020-04-22 19:09:39 +0530

    fix typo and changed batch size

--- jovian/tests/callbacks/test_fastai.py
+++ jovian/tests/callbacks/test_fastai.py
@@ -2,7 +2,7 @@ import os
 import sys
 from unittest import mock
 from unittest.mock import ANY
-from pathlib import Path
+from pathlib import PosixPath
 import pytest
 try:
     from fastai import *
@@ -16,34 +16,27 @@ except ImportError:
 @pytest.fixture
 def learn():
     tfms = get_transforms(do_flip=False)
-    data = (ImageList.from_folder("./jovian/tests/resources/mnist_tiny")
+    data = (ImageList.from_folder(PosixPath("./jovian/tests/resources/mnist_tiny"))
             .split_by_folder()
             .label_from_folder()
             .transform(tfms, size=32)
             .databunch()
             .normalize(imagenet_stats))
+    data.batch_size = 2
 
     learn = cnn_learner(data, models.resnet18, metrics=accuracy)
     return learn
 
 
+@pytest.mark.skipif(sys.version_info < (3, 6), reason="requires python3.6 or higher")
 @mock.patch("jovian.callbacks.fastai.log_hyperparams")
 @mock.patch("jovian.callbacks.fastai.log_metrics")
 def test_on_train_begin_and_on_epoch_end(mock_log_metrics, mock_log_hyperparams, learn):
     jvn_cb = JovianFastaiCallback(learn, 'res18')
-    learn.fit_one_cycle(1, callbacks=jvn_cb)
+    learn.fit_one_cycle(2, callbacks=jvn_cb)
 
-    mock_log_hyperparams.assert_called_with({
-        'epochs': 1,
-        'batch_size': 64,
-        'loss_func': 'CrossEntropyLoss()',
-        'opt_func': 'torch.optim.adam.Adam',
-        'weight_decay': 0.01,
-        'learning_rate': '0.003',
-        'arch_name': 'res18'})
+    mock_log_hyperparams.assert_called_with(
+        {'epochs': 2, 'batch_size': 2, 'loss_func': 'CrossEntropyLoss()', 'opt_func': 'torch.optim.adam.Adam',
+         'weight_decay': 0.01, 'learning_rate': '0.003', 'arch_name': 'res18'})
 
-    mock_log_metrics.assert_called_with({
-        'epoch': 0,
-        'train_loss': ANY,
-        'valid_loss': ANY,
-        'accuracy': ANY})
+    mock_log_metrics.assert_called_with({'epoch': 1, 'train_loss': ANY, 'valid_loss': ANY, 'accuracy': ANY})
