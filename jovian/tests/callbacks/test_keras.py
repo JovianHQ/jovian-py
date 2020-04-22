bbeb289014ed636246c4a410e4c8ed86cf6e5949
Rohit Sanjay <sanjay.rohit2@gmail.com>
2020-04-22 16:22:36 +0530

    fix import error

--- jovian/tests/callbacks/test_keras.py
+++ jovian/tests/callbacks/test_keras.py
@@ -2,9 +2,9 @@ from unittest import mock
 from unittest.mock import ANY
 import pytest
 import sys
+import numpy as np
 
 try:
-    import numpy as np
     from keras.layers import Dense, Dropout
     from keras.models import Sequential
 
