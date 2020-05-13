7b3c599f4af8d5d68ab571826c4961670b23e6b9
praj22 <prajwal.prasahanth22@gmail.com>
2020-05-13 11:54:55 +0530

    remove jovianrc

---
+++ jovian/tests/utils/test_slug.py
@@ -0,0 +1,12 @@
+from jovian.utils.slug import get_current_slug, set_current_slug
+
+
+@mock.patch("jovian.utils.slug.get_notebook_slug", return_value="rohit/time-series-new")
+def test_set_current_slug():
+    assert
+
+
+def test_get_current_slug()
+
+
+assert
