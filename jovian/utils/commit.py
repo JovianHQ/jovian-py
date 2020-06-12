a39077010daf4c563251a91535246ba8e0651185
Prajwal Prashanth <prajwal.prashanth22@gmail.com>
2020-06-13 00:40:41 +0530

    WIP: Commit from Kaggle (#188)
    * temp
    
    * save cells to file for kaggle, empty check
    
    * save file in kernel execute
    
    * bug fix
    
    * bump dev
    
    * Use filename
    
    * Commit from javascript
    
    * Show success message
    
    * bump version
    
    * remove comments
    
    Co-authored-by: praj22 <prajwal.prasahanth22@gmail.com>
    Co-authored-by: Aakash N S <aakashns.sky@gmail.com>

--- jovian/utils/commit.py
+++ jovian/utils/commit.py
@@ -1,5 +1,6 @@
 import glob
 import os
+import json
 from time import sleep
 
 import click
@@ -8,6 +9,7 @@ from jovian.utils.constants import DEFAULT_EXTENSION_WHITELIST, FILENAME_MSG
 from jovian.utils.credentials import read_creds, read_webapp_url
 from jovian.utils.environment import CondaError, upload_conda_env, upload_pip_env
 from jovian.utils.jupyter import get_notebook_name, in_notebook, save_notebook
+from jovian.utils.kaggle import perform_kaggle_commit
 from jovian.utils.logger import log
 from jovian.utils.misc import get_file_extension, is_uuid, urljoin
 from jovian.utils.rcfile import get_cached_slug, get_notebook_slug, reset_notebook_slug, set_notebook_slug
@@ -135,6 +137,16 @@ def commit(message=None,
     filename = _parse_filename(filename)
     if filename is None:
         log(FILENAME_MSG, error=True)
+        return
+
+    # Commit from Kaggle (After many bug reports of empty notebook)
+    if filename == '__notebook_source__.ipynb':
+        log("Detected Kaggle notebook...")
+        if not project:
+            log("Please provide the project argument e.g. jovian.commit(project='my-project')", error=True)
+            return
+
+        perform_kaggle_commit(project)
         return
 
     # Ensure that the file exists
