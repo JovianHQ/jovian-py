66512679d576d16224f7bca896887d9b3188e9f0
praj22 <prajwal.prasahanth22@gmail.com>
2020-07-01 01:32:49 +0530

    get and set colab id

--- jovian/utils/colab.py
+++ jovian/utils/colab.py
@@ -1,5 +1,17 @@
 import sys
 
+_colab_file_id = None
+
 
 def in_colab():
     return 'google.colab' in sys.modules
+
+
+def set_colab_file_id(colab_file_id):
+    global _colab_file_id
+    _colab_file_id = file_id
+
+
+def get_colab_file_id():
+    global _colab_file_id
+    return _colab_file_id
