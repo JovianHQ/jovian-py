34be63e5c089f6353466e66f1dacb6702a89e2ff
praj22 <prajwal.prasahanth22@gmail.com>
2020-09-08 16:34:35 +0530

    single arg, removing excess logs

--- jovian/utils/submit.py
+++ jovian/utils/submit.py
@@ -11,13 +11,15 @@ POST_API_URL = '/learn/course/{}/section/{}/make_submission'
 ASSIGNMENT_PAGE_URL = '/learn/{}/assignment/{}'
 
 
-def submit(course_slug=None, assignment_slug=None, notebook_url=None, **kawrgs):
+def submit(assignment=None, notebook_url=None, **kwargs):
     """ Performs jovian.commit and makes a assignment submission with the uploaded notebook.
     """
-    if not course_slug or not assignment_slug:
-        log("Missing Course Slug or Assignment Slug", error=True)
+    if not assignment:
+        log("Please provide assignment argument", error=True)
+        return
 
-    nb_url = notebook_url if notebook_url else commit(**kawrgs)
+    course_slug, assignment_slug = assignment.split("/")
+    nb_url = notebook_url if notebook_url else commit(**kwargs)
 
     if nb_url:
         data = {
@@ -25,19 +27,14 @@ def submit(course_slug=None, assignment_slug=None, notebook_url=None, **kawrgs):
         }
         auth_headers = _h()
 
-        log('Submitting assignment....')
+        log('Submitting assignment..')
         post_url = POST_API_URL.format(course_slug, assignment_slug)
         res = post(url=_u(post_url),
                    json=data,
                    headers=auth_headers)
 
         if res.status_code == 200:
-            assignment = res.json()['data'].get('assignment')
-            if assignment:
-                assignment = assignment.get('assignment_url')
             assignment_page_url = ASSIGNMENT_PAGE_URL.format(course_slug, assignment_slug)
-
-            log('Submitted assignment - {}'.format(assignment))
             log('Verify your submission at {}'.format(urljoin(read_webapp_url(), assignment_page_url)))
         else:
             log('Jovian submit failed. {}'.format(pretty(res)), error=True)
