7f1ec3a958d5fbdf314b3eeb31cc83e62a660d63
praj22 <prajwal.prasahanth22@gmail.com>
2020-09-11 14:21:06 +0530

    support for short slug

--- jovian/utils/submit.py
+++ jovian/utils/submit.py
@@ -8,17 +8,24 @@ from jovian.utils.request import post, pretty
 from jovian.utils.shared import _u
 
 POST_API_URL = '/learn/course/{}/section/{}/make_submission'
+POST_API_URL_SHORT_SLUG = '/learn/section-short-slug/{}/make_submission'
 ASSIGNMENT_PAGE_URL = '/learn/{}/assignment/{}'
 
 
-def submit(assignment=None, notebook_url=None, **kwargs):
+def submit(assignment=None, assignment_short_slug=None, notebook_url=None, **kwargs):
     """ Performs jovian.commit and makes a assignment submission with the uploaded notebook.
     """
-    if not assignment:
-        log("Please provide assignment argument", error=True)
+    if not assignment and not assignment_short_slug:
+        log("Please provide assignment or assignment_short_slug argument", error=True)
         return
 
-    course_slug, assignment_slug = assignment.split("/")
+    if assignment:
+        course_slug, assignment_slug = assignment.split("/")
+        post_url = POST_API_URL.format(course_slug, assignment_slug)
+
+    if assignment_short_slug:
+        post_url = POST_API_URL_SHORT_SLUG.format(assignment_short_slug)
+
     nb_url = notebook_url if notebook_url else commit(**kwargs)
 
     if nb_url:
@@ -28,12 +35,15 @@ def submit(assignment=None, notebook_url=None, **kwargs):
         auth_headers = _h()
 
         log('Submitting assignment..')
-        post_url = POST_API_URL.format(course_slug, assignment_slug)
         res = post(url=_u(post_url),
                    json=data,
                    headers=auth_headers)
 
         if res.status_code == 200:
+            data = res.json()['data']
+            course_slug = data.get('course_slug')
+            assignment_slug = data.get('assignment_slug')
+
             assignment_page_url = ASSIGNMENT_PAGE_URL.format(course_slug, assignment_slug)
             log('Verify your submission at {}'.format(urljoin(read_webapp_url(), assignment_page_url)))
         else:
