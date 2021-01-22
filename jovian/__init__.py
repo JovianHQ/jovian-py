630f3eea2ef41a9dcbd4ea2320c1dc0f20e310e4
Siddhant Ujjain <siddhant@jovian.ml>
2021-01-22 18:19:23 +0530

    Add set_colab_id and set_project as top level functions (#237)

--- jovian/__init__.py
+++ jovian/__init__.py
@@ -4,8 +4,10 @@ from jovian.utils.records import (log_hyperparams, log_metrics, log_dataset,
                                   log_git, reset, get_records)
 from jovian.utils.slack import notify
 from jovian.utils.submit import submit
+from jovian.utils.colab import set_colab_file_id as set_colab_id
 from jovian.utils.configure import reset_config, configure
 from jovian.utils.initialize import _initialize_jovian
 from jovian.utils.misc import version
+from jovian.utils.rcfile import set_project
 
 _initialize_jovian()
