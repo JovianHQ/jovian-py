a90b7c0d11abf6343a9fb07622dda6e81d674af9
Siddhant <siddhant.ujjain@gmail.com>
2021-01-25 16:46:04 +0530

    Fix deploy alert version

--- deployAlert.sh
+++ deployAlert.sh
@@ -2,13 +2,13 @@
 if [ ! -z $SLACK_DEPLOYMENT_HOOK_URL ]
 then
   curl -X POST -H 'Content-type: application/json' --data "{
-    'text': 'Deploying Library [${1}] [VERSION: ${4}]!:male-technologist:',
+    'text': 'Deploying Library [${1}] [VERSION: ${2}]!:male-technologist:',
     'blocks':[
     {
       'type': 'section',
       'text': {
         'type': 'mrkdwn',
-        'text': 'Deploying Library [${1}] [VERSION: ${4}]!:male-technologist:',
+        'text': 'Deploying Library [${1}] [VERSION: ${2}]!:male-technologist:',
       }
     }
   ]}" $SLACK_DEPLOYMENT_HOOK_URL
