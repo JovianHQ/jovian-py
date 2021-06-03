ee1b93abebd00af00570974eef31f60028311319
Rohit Sanjay <sanjay.rohit2@gmail.com>
2021-06-03 12:25:34 +0530

    refactor

--- jovian/utils/git.py
+++ jovian/utils/git.py
@@ -1,12 +1,6 @@
 """Git related utilities"""
 import os
-import re
 import subprocess
-from urllib.parse import urlparse
-
-from jovian.utils.credentials import get_api_key, read_api_url
-
-HOME = os.path.expanduser('~')
 
 
 def is_git():
@@ -40,73 +34,9 @@ def commit(message):
     return os.system('cd ' + get_repository_root() + ' && git add . &&  git commit -a -m "' + message + '" && cd -')
 
 
-def git_push(remote):
-    """Push the branch to remote"""
-    return os.system("git push {} {}".format(remote, get_branch()))
-
-
-def remote_update(remote):
-    return os.system("git remote update {}".format(remote)) == 0
-
-
-def is_up_to_date():
-    return "behind" not in os.popen("git status -uno").read().splitlines()[1]
-
-
-def is_jovian_remote(remote):
-    return urlparse(read_api_url()).netloc == urlparse(remote).netloc
-
-
-def insert_git_credential(username):
-    api_url = urlparse(read_api_url())
-    password = get_api_key()
-    git_creds_path = fetch_git_credential_path(username)
-    with open(git_creds_path, 'w') as f:
-        f.write(construct_git_creds(api_url, username, password))
-
-    return os.system("git config --global credential.helper 'store --file {}'".format(git_creds_path))
-
-
-def construct_git_creds(api_url, username, password):
-    return api_url.scheme + "://" + username + ":" + password + "@" + api_url.netloc + "\n"
-
-
-def fetch_git_credential_path(username):
-    return os.path.join(HOME, '.git-jovian-credentials-{}'.format(username))
-
-
-def check_write_access():
-    return os.system("git push origin " + get_branch() + " --dry-run") == 0
-
-
-def is_index_dirty():
-    return os.popen('git status --porcelain').read().strip() != ""
-
-
-def clone(username, title, destination=None):
-    api_url = read_api_url()
-    insert_git_credential(username)
-    clone_url = os.path.join(api_url, username, title)
-    return os.system('git clone {} {}'.format(clone_url, destination or '')) == 0
-
-
-def get_jovian_remote(username, title):
-    remotes = os.popen('git remote').read().splitlines()
-    api_url = read_api_url()
-    api_net_loc = urlparse(api_url).netloc
-    project = username + "/" + title
-
-    for remote in remotes:
-        remote_url = urlparse(get_remote_url(remote))
-        remote_project = _remove_git_suffix(remote_url.path.strip('/'))
-
-        if remote_url.netloc == api_net_loc and remote_project == project:
-            return remote
-    return None
-
-
-def get_remote_url(remote):
-    return os.popen('git remote get-url {}'.format(remote)).read().strip()
+def git_push():
+    """Push the branch to origin"""
+    return os.system("git push origin " + get_branch())
 
 
 def git_commit_push(message):
@@ -126,7 +56,3 @@ def get_relative_path():
     root_dir = get_repository_root()
     file_dir = os.path.abspath('')
     return os.path.relpath(file_dir, root_dir)
-
-
-def _remove_git_suffix(project_title):
-    return re.sub(r"\.git$", "", project_title)
